9dd2b637b41315fd442efd69da7b6731a480f3a2
vladok0707 <vladok-warface@mail.ru>
2019-11-13 09:42:18 +0300

    Laba5

---
+++ Laba5_WebDriver/SeleniumWebdriver/SeleniumWebdriver/Tests/SearchTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumWebdriver.Tests
+{
+    [TestFixture]
+    public class SearchTest : TestBase
+    {
+        string ErrorEmail = "Некорректный адрес электронной почты!";
+
+        [Test]
+        public void SearchWithEqualDepartureAndDestinationPlace()
+        {
+            IWebElement link = _webDriver.FindElement(By.XPath("//a[@class='dropdown-toggle']"));
+            link.Click();
+            link = _webDriver.FindElement(By.XPath("//i[@class='fa fa-user-plus']"));
+            link.Click();
+            link = _webDriver.FindElement(By.XPath("//a[@class='button btn-primary button_oc btn']"));
+            link.Click();
+            Thread.Sleep(1000);
+            link = _webDriver.FindElement(By.XPath("//div[@class='col-sm-10']//div[@data-rule='regexp']"));
+            Assert.AreEqual(ErrorEmail, link.Text);
+        } 
+
+        string urlVkDomsBy= "https://vk.com/doms.market";
+        [Test]
+        public void pressVkIconInFooter()
+        {
+            IWebElement link = _webDriver.FindElement(By.XPath("//i[@class='fa fa-vk']"));
+            link.Click();
+            string urlVk = _webDriver.Url;
+            Assert.AreEqual(urlVkDomsBy, urlVk);
+        }
+    }
+}
